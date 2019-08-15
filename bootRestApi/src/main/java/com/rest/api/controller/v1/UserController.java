457b787dddc4b7d6d16d237fea8fe2979fe9592f
gyusuk <gyusuk6394@gmail.com>
2019-08-15 14:58:16 +0900

    [ADD] add Swagger

--- bootRestApi/src/main/java/com/rest/api/controller/v1/UserController.java
+++ bootRestApi/src/main/java/com/rest/api/controller/v1/UserController.java
@@ -2,27 +2,31 @@ package com.rest.api.controller.v1;
 
 import com.rest.api.entity.User;
 import com.rest.api.repo.UserJpaRepo;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(tags = {"1. User"})
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/v1")
 public class UserController {
     private final UserJpaRepo userJpaRepo;
 
+    @ApiOperation(value = "회원 조회", notes = "모든 회원을 조회한다")
     @GetMapping(value = "/user")
     public List<User> findAllUser() {
         return userJpaRepo.findAll();
     }
 
+    @ApiOperation(value = "회원 입력", notes = "회원을 입력한다.")
     @PostMapping(value = "/user")
-    public User save() {
+    public User save(@ApiParam(value = "회원아이디", required = true) @RequestParam String uid,
+                     @ApiParam(value = "회원이름", required = true) @RequestParam String name) {
         User user = User.builder()
                 .uid("gyuseok@gmail.com")
                 .name("규석")
