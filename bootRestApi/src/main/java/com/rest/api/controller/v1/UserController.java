8c5f132f384d24c74ff0ed8ae70e339bb682a5e6
gyusuk <gyusuk6394@gmail.com>
2019-08-16 20:45:52 +0900

    [ADD] add implement customer exception and document folder

--- bootRestApi/src/main/java/com/rest/api/controller/v1/UserController.java
+++ bootRestApi/src/main/java/com/rest/api/controller/v1/UserController.java
@@ -1,5 +1,6 @@
 package com.rest.api.controller.v1;
 
+import com.rest.api.advice.exception.CUserNotFoundException;
 import com.rest.api.entity.User;
 import com.rest.api.model.response.CommonResult;
 import com.rest.api.model.response.ListResult;
@@ -29,9 +30,10 @@ public class UserController {
 
     @ApiOperation(value = "회원 단건 조회", notes = "userId로 회원을 조회한다")
     @GetMapping(value = "/user/{msrl}")
-    public SingleResult<User> findUserById(@ApiParam(value = "회원ID", required = true) @PathVariable long msrl) {
+    public SingleResult<User> findUserById(@ApiParam(value = "회원ID", required = true) @PathVariable long msrl,
+                                           @ApiParam(value = "언어", defaultValue = "ko") @RequestParam String lang) {
         // 결과데이터가 단일건인경우 getBasicResult를 이용해서 결과를 출력한다.
-        return responseService.getSingleResult(userJpaRepo.findById(msrl).orElse(null));
+        return responseService.getSingleResult(userJpaRepo.findById(msrl).orElseThrow(CUserNotFoundException::new));
     }
 
     @ApiOperation(value = "회원 입력", notes = "회원을 입력한다.")
