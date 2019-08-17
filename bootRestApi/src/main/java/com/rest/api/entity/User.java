6eddd7288b9790962c5889595251ff107352d3e9
gyusuk <gyusuk6394@gmail.com>
2019-08-17 18:08:11 +0900

    [ADD] add Spring Security

--- bootRestApi/src/main/java/com/rest/api/entity/User.java
+++ bootRestApi/src/main/java/com/rest/api/entity/User.java
@@ -1,7 +1,16 @@
 package com.rest.api.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Builder
 @Entity
@@ -9,14 +18,54 @@ import javax.persistence.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "user")
-public class User {
-    @Id
+public class User implements UserDetails {
+    @Id // pk
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long msrl;
-
     @Column(nullable = false, unique = true, length = 30)
     private String uid;
-
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Column(nullable = false, length = 100)
+    private String password;
     @Column(nullable = false, length = 100)
     private String name;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Builder.Default
+    private List<String> roles = new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    }
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Override
+    public String getUsername() {
+        return this.uid;
+    }
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- bootRestApi/src/main/java/com/rest/api/repo/UserJpaRepo.java
+++ bootRestApi/src/main/java/com/rest/api/repo/UserJpaRepo.java
@@ -2,5 +2,11 @@ package com.rest.api.repo;
 
 import com.rest.api.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UserJpaRepo extends JpaRepository<User, Long> {}
+import java.util.Optional;
+
+@Repository
+public interface UserJpaRepo extends JpaRepository<User, Long> {
+    Optional<User> findByUid(String email);
+}
